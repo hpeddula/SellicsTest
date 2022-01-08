548cb32569c9bc409d663074a646c93ccfd899d6
Harsha <hpeddula@outlook.com>
2022-01-08 10:42:35 +0530

    Using redux persist to persist data on refresh

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,32 @@
 import { configureStore } from '@reduxjs/toolkit';
 import appImageSlice from '../slice/appImageSlice';
+import { persistStore, persistReducer ,FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER} from 'redux-persist' // imports from redux-persist
+import storage from 'redux-persist/lib/storage/session'
+
+const persistConfig = { // configuration object for redux-persist
+  key: 'root',
+  storage, // define which storage to use
+}
+
+export const persistedReducer = persistReducer(persistConfig, appImageSlice) // create a persisted reducer
 
 export const store = configureStore({
   reducer: {
-    appImageSlice
+    appImages:persistedReducer
   },
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  })
 })
-
+export const persistor = persistStore(store)
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
